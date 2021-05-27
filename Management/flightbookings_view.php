c2027303c03561061b0fdbf800919514498650a7
Alisha Anand <55910525+anandalisha@users.noreply.github.com>
2021-05-28 00:51:24 +0530

    added admin basic structure

---
+++ Management/flightbookings_view.php
@@ -0,0 +1,172 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Admin Panel | Flights </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+<style>
+body{
+    font-family: Courgette;
+}
+</style>
+</head>
+<?php
+$servername = "localhost";
+$username="root";
+$password="";
+$dbname="projectmeteor";
+$bookingID="";
+$hotelName="";
+$date="";
+$username="";
+$cancelled="";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+//connect to mysql database
+try{
+	$conn = mysqli_connect($servername,$username,$password,$dbname);
+}catch(MySQLi_Sql_Exception $ex){
+	echo("");
+}
+//get data from the form
+function getData()
+{
+	$data = array();
+
+	$data[1]=$_POST['username'];
+	$data[2]=$_POST['date'];
+	$data[3]=$_POST['cancelled'];
+	$data[4]=$_POST['origin'];
+    $data[4]=$_POST['destination'];
+    $data[4]=$_POST['passengers'];
+    $data[4]=$_POST['type'];
+	return $data;
+}
+//search
+if(isset($_POST['search']))
+{
+	$info = getData();
+	$search_query="SELECT * FROM users WHERE bookingID = '$info[0]'";
+	$search_result=mysqli_query($conn, $search_query);
+		if($search_result)
+		{
+			if(mysqli_num_rows($search_result))
+			{
+				while($rows = mysqli_fetch_array($search_result))
+				{
+					$bookingID = $rows['bookingID'];
+					$username = $rows['hotelName'];
+					$date = $rows['date'];
+					$cancelled = $rows['cancelled'];
+					$origin = $rows['origin'];
+                    $destination = $rows['destination'];
+                    $passengers = $rows['passengers'];
+                    $type = $rows['type'];
+
+
+				}
+			}else{
+				echo("No data are available");
+			}
+		} else{
+			echo("Result error");
+		}
+
+}
+?>
+
+<body>
+	<div class="container-fliud">
+        <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav">
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+     </button>
+      <a class="navbar-brand" href="http://localhost/management/"> Tourism Management System</a>
+    </div>
+     <div class="collapse navbar-collapse" id="nav">
+    <ul class="nav navbar-nav">
+        <li><a href="Home.html">HOME</a></li>
+		<li><a href="users_add.php">USERS</a></li>
+              <li><a href="hotels_add.php">ADD HOTELS</a></li>
+              <li><a href="hotelbookings_view.php">HOTEL BOOKINGS</a></li>
+              <li><a href="flights_add.php">ADD FLIGHTS</a></li>
+              <li><a href="flightbookings_view.php">FLIGHT BOOKINGS</a></li>
+              <li><a href="trains_add.php">ADD TRAINS</a></li>
+              <li><a href="trainbookings_view.php">TRAIN BOOKINGS</a></li>
+    </ul>
+  </div>
+</div>
+</nav>
+      </div>
+
+
+	
+
+    <div class="col-lg-8">
+			<h2>FLIGHT BOOKING DETAILS</h2>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projectmeteor";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT bookingID,username,date,cancelled,origin,destination,passengers,type FROM flightbookings";
+$result = $conn->query($sql);
+
+echo "<table border='1'>
+<tr>
+<th>Booking ID</th>
+<th>Username</th>
+<th>Date</th>
+<th>Cancelled</th>
+<th>Origin</th>
+<th>Destination</th>
+<th>Passengers</th>
+<th>Tpye</th>
+
+</tr>";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+      echo "<tr>";
+echo "<td>" . $row['bookingID'] . "</td>";
+echo "<td>" . $row['username'] . "</td>";
+echo "<td>" . $row['date'] . "</td>";
+echo "<td>" . $row['cancelled'] . "</td>";
+echo "<td>" . $row['origin'] . "</td>";
+echo "<td>" . $row['destination'] . "</td>";
+echo "<td>" . $row['passengers'] . "</td>";
+echo "<td>" . $row['type'] . "</td>";
+
+echo "</tr>";
+
+
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+</div>
+</div>
+</body>
+</html>
